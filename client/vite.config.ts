9e33e74788e08f3f0e6f2dad71008547d8c1e453
gautamkumar1 <gautamraaz936@gmail.com>
2024-06-03 15:53:46 +0530

    proxy added

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,11 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server:{
+    proxy:{
+      '/api':'https://invoice-generator-2qks.onrender.com'
+    }
+  },
   plugins: [react()],
   resolve: {
     alias: {
